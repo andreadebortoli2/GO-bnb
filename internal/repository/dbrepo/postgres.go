c1fcd6f4078cccb1592a711808ebdd7239e99ff4
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-09-03 19:37:59 +0200

    add search availability for room

--- internal/repository/dbrepo/postgres.go
+++ internal/repository/dbrepo/postgres.go
@@ -66,3 +66,30 @@ func (m postgresDBRepo) InsertRoomRestriction(r models.RoomRestriction) error {
 
 	return nil
 }
+
+// SearchAvailabilityByDates return true if availability exists for room id, and false if no avaliability exists
+func (m postgresDBRepo) SearchAvailabilityByDates(start, end time.Time, roomID int) (bool, error) {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	stmt := `select
+				count(id)
+			from
+				room_restrictions
+			where
+				room_id = $1 
+				$2 < end_date and $3 > start_date`
+
+	var numRows int
+	row := m.DB.QueryRowContext(ctx, stmt, roomID, start, end)
+	err := row.Scan(&numRows)
+	if err != nil {
+		return false, err
+	}
+
+	if numRows == 0 {
+		return true, nil
+	}
+	return false, nil
+}
