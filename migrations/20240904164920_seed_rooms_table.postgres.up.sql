a3e2847c5b3be8fa9df9dbacd82f5219b21c87f3
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-09-04 19:23:41 +0200

    add rooms and restrictions seeders

---
+++ migrations/20240904164920_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.rooms (room_name, created_at, updated_at)
+VALUES 
+    ('General''s Quarters', '2024-09-04 00:00:00', '2024-09-04 00:00:00'),
+    ('Major''s Suite', '2024-09-04 00:00:00', '2024-09-04 00:00:00');
---
+++ migrations/20240904165615_seed_restrictions_table.postgres.up.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.restrictions (restriction_name, created_at, updated_at)
+VALUES 
+    ('Reservation', '2024-09-04 00:00:00', '2024-09-04 00:00:00'),
+    ('Owner Block', '2024-09-04 00:00:00', '2024-09-04 00:00:00');
