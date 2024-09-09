b9f531655d0943ec7c93c832edf740b168c101f3
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-09-09 16:53:40 +0200

    add db user crud(show and update)

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -9,9 +9,14 @@ import (
 type DatabaseRepo interface {
 	AllUsers() bool
 
+	// reservation funcs
 	InsertReservation(res models.Reservation) (int, error)
 	InsertRoomRestriction(r models.RoomRestriction) error
 	SearchAvailabilityByDatesByRoomID(start, end time.Time, roomID int) (bool, error)
 	SearchAvailabilityForAllRooms(start, end time.Time) ([]models.Room, error)
 	GetRoomByID(id int) (models.Room, error)
+	// authentication funcs
+	GetUserByID(id int) (models.User, error)
+	UpdateUser(u models.User) error
+	Authenticate(email, testPassword string) (int, string, error)
 }
