52349bf52a9c95f7887fc841d5da31a61fa26bc6
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-09-11 18:32:05 +0200

    add show reservation page with a form and edit handlers and layout to correctly display the page

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -580,6 +580,7 @@ func (m *Repository) AdminShowReservation(w http.ResponseWriter, r *http.Request
 	render.Templates(w, r, "admin-reservations-show.page.tmpl", &models.TemplateData{
 		StringMap: stringMap,
 		Data:      data,
+		Form:      forms.New(nil),
 	})
 }
 
