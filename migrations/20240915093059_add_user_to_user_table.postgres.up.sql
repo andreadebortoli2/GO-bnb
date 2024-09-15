73f44bb3e5c91c1fa517c5725174e03fb4a22bdf
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-09-15 11:43:07 +0200

    add seeder for admin user

---
+++ migrations/20240915093059_add_user_to_user_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.users
+(first_name, last_name, email, "password", access_level, created_at, updated_at)
+VALUES('Admin', 'Admin', 'admin@admin.com', '$2a$12$P08FcZaeqNiEx9.WRlV.Z.22r7RpTIg9NEK92SeLMKhjbuprYjnN.', 3, '2024-01-01 00:00:00.000', '2024-01-01 00:00:00.000');
